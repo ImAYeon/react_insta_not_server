07c69dbfa7d8b7eb6c88617bcc509a5459157599
ayeon <gudwodjssl@gmail.com>
2022-06-03 17:33:06 +0900

    react project

---
+++ src/data/User.js
@@ -0,0 +1,23 @@
+export const Users = [
+  {
+    id: 0,
+    name: "kim",
+    userId: "kim",
+    img: "/img/profile/세영.jpg",
+    password: "1234",
+  },
+  {
+    id: 1,
+    name: "park",
+    userId: "park",
+    img: "/img/profile/세영2.jpg",
+    password: "1234",
+  },
+  {
+    id: 2,
+    name: "lee",
+    userId: "lee",
+    img: "/img/profile/세영3.jpg",
+    password: "1234",
+  },
+];
